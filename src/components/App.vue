c1dd40c1c108c9c5871e5ffefbc8acf827b0b9c6
Satwik Kambham <satwik.kambham@gmail.com>
2023-10-07 15:51:13 +0530

    Starter project with vite, vue and tailwind

---
+++ src/components/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from 'vue'
+const greeting = ref('Hello World!')
+</script>
+
+<template>
+  <input v-model="greeting" />
+  <p class="text-3xl font-bold underline">{{ greeting }}</p>
+</template>
